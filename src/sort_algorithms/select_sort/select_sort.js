37c46f7802562d49de12c35b65f91ce1e15d9e6b
MICHAŁ SOBIECKI <kontakt@codeshaker.pl>
2019-08-13 19:01:17 +0200

    OPTIMALIZE

--- src/sort_algorithms/select_sort/select_sort.js
+++ src/sort_algorithms/select_sort/select_sort.js
@@ -1,11 +1,10 @@
 function find_smallest(arr) {
   let smallest_index = 0;
   let smallest = arr[smallest_index];
-
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < smallest) {
-      smallest = arr[i];
       smallest_index = i;
+      smallest = arr[smallest_index];
     }
   }
   return smallest_index;
@@ -14,8 +13,7 @@ function find_smallest(arr) {
 export function select_sort(arr) {
   const newArr = [];
   while (arr.length > 0) {
-    const smallest = find_smallest(arr);
-    newArr.push(...arr.splice(smallest, 1));
+    newArr.push(...arr.splice(find_smallest(arr), 1));
   }
   return newArr;
 }
