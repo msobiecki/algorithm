c3aab59e1ed4245106276f7bab5c8d27ed021033
MICHAŁ SOBIECKI <kontakt@codeshaker.pl>
2019-08-13 19:00:06 +0200

    SELECT SORT IMPLEMENTED

--- src/sort_algorithms/select_sort/select_sort.js
+++ src/sort_algorithms/select_sort/select_sort.js
@@ -1,5 +1,23 @@
-export function select_sort() {
-  console.log('SELECT_SORT');
+function find_smallest(arr) {
+  let smallest_index = 0;
+  let smallest = arr[smallest_index];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallest_index = i;
+    }
+  }
+  return smallest_index;
+}
+
+export function select_sort(arr) {
+  const newArr = [];
+  while (arr.length > 0) {
+    const smallest = find_smallest(arr);
+    newArr.push(...arr.splice(smallest, 1));
+  }
+  return newArr;
 }
 
 export default select_sort;
---
+++ src/sort_algorithms/select_sort/select_sort.test.js
@@ -0,0 +1,21 @@
+import select_sort from './select_sort';
+
+describe('[TEST SELECT SORT] -', () => {
+  it('should return sorted array', () => {
+    const list = [1, 3, 8, 7, 6];
+    const result = select_sort(list);
+    expect(result).toStrictEqual([1, 3, 6, 7, 8]);
+  });
+
+  it('should return sorted array', () => {
+    const list = [1, 1, 1, 8, 1, 1, 1, 1, 1];
+    const result = select_sort(list);
+    expect(result).toStrictEqual([1, 1, 1, 1, 1, 1, 1, 1, 8]);
+  });
+
+  it('should return sorted array', () => {
+    const list = [1, 2, 3];
+    const result = select_sort(list);
+    expect(result).toStrictEqual([1, 2, 3]);
+  });
+});
