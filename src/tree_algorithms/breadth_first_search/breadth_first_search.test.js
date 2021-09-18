6b29764f5424092a911597e4ccd6950013615afe
CODESHAKER Michal Sobiecki <kontakt@codeshaker.pl>
2021-09-18 13:30:01 +0200

    New: Update tests

--- src/tree_algorithms/breadth_first_search/breadth_first_search.test.js
+++ src/tree_algorithms/breadth_first_search/breadth_first_search.test.js
@@ -1,33 +1,33 @@
 import BinarySearchTree from "./../../tree/binary_search_tree";
 import breadth_first_search from "./breadth_first_search";
 
-describe("[TEST GRAPH] -", () => {
+describe("[TEST BREADTH FIRST SEARCH TREE] -", () => {
   it("should return breadth first search found value in binary search tree", () => {
-    const Graph = new BinarySearchTree();
-    Graph.insert(10);
-    Graph.insert(4);
-    Graph.insert(17);
-    Graph.insert(1);
-    Graph.insert(9);
-    Graph.insert(12);
-    Graph.insert(18);
-    const root = Graph.getRootNode();
+    const Tree = new BinarySearchTree();
+    Tree.insert(10);
+    Tree.insert(4);
+    Tree.insert(17);
+    Tree.insert(1);
+    Tree.insert(9);
+    Tree.insert(12);
+    Tree.insert(18);
+    const root = Tree.getRootNode();
     expect(breadth_first_search(root, 18)).toStrictEqual([10, 4, 17, 1, 9, 12, 18]);
   });
 
   it("should return breadth first search not found in binary search tree", () => {
-    const Graph = new BinarySearchTree();
-    Graph.insert(15);
-    Graph.insert(25);
-    Graph.insert(10);
-    Graph.insert(7);
-    Graph.insert(22);
-    Graph.insert(17);
-    Graph.insert(13);
-    Graph.insert(5);
-    Graph.insert(9);
-    Graph.insert(27);
-    const root = Graph.getRootNode();
+    const Tree = new BinarySearchTree();
+    Tree.insert(15);
+    Tree.insert(25);
+    Tree.insert(10);
+    Tree.insert(7);
+    Tree.insert(22);
+    Tree.insert(17);
+    Tree.insert(13);
+    Tree.insert(5);
+    Tree.insert(9);
+    Tree.insert(27);
+    const root = Tree.getRootNode();
     expect(breadth_first_search(root, 44)).toStrictEqual(null);
   });
 });
