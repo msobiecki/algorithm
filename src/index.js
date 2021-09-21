e5d63afe73b0e2b6c465b86d7e5f2f572581ad9c
CODESHAKER Michal Sobiecki <kontakt@codeshaker.pl>
2021-09-21 17:03:31 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import DeepCopy from './deep_copy';
-import Search from './search_algorithms';
-import Sort from './sort_algorithms';
-import Recursion from './recursion';
-import ShortcutTables from './shortcut_tables';
+import DeepCopy from "./deep_copy";
+import Search from "./search_algorithms";
+import Sort from "./sort_algorithms";
+import Recursion from "./recursion";
+import ShortcutTables from "./shortcut_tables";
 import Tree from "./tree";
 import TreeSearch from "./tree_algorithms";
 
